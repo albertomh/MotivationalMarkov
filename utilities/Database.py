66881fb8b00ff58d974fff8ef2fd8b6523bf1d81
Alberto Morón Hernández <alberto.moronh@gmail.com>
2018-07-23 22:52:51 +0100

    Add the connect() method to the Database.

--- utilities/Database.py
+++ utilities/Database.py
@@ -4,13 +4,27 @@
 """ Set up a SQLite database using the given schema and create tables.
 """
 
+import sqlite3
+
 
 class Database:
     def __init__(self, path):
         self.path_to_database_file = path
 
+    """
+    Return a connection to the SQLite database.
+    """
+
+    def connect(self):
+        try:
+            return sqlite3.connect(self.path_to_database_file)
+        except sqlite3.Error as error:
+            print(error)
+
+        return None
+
     def main(self):
-        return
+        self.connect()
 
 
 if __name__ == "__main__":
