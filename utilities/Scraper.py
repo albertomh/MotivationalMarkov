860d8cbeededd574b4695317efde87fb077dbbb9
Alberto Morón Hernández <alberto.moronh@gmail.com>
2018-07-27 07:58:42 +0100

    Scrape the homepage's sidebar for links to month subdirectories.

--- utilities/Scraper.py
+++ utilities/Scraper.py
@@ -5,6 +5,9 @@
     Populates the following tables: 'months', 'articles'.
 """
 
+import re
+import requests
+from bs4 import BeautifulSoup
 from utilities.Database import Database
 from utilities.Helpers import Helpers as Helper
 
@@ -27,6 +30,21 @@ class Scraper:
         if last_month is None:
             last_month = (0, '1970', '01')
         last_month_timestamp = Helper.to_timestamp('/'.join(last_month[1:]), '%Y/%m')
+
+        # Scrape the homepage's sidebar for links to month subdirectories.
+        page = requests.get(self.homepage).text
+        for link in BeautifulSoup(page, 'html.parser').findAll('a', attrs={'href': re.compile("/\d{4}/\d{2}$")}):
+            url = link.get('href')
+
+            human_readable = url[-7:]
+            link_timestamp = Helper.to_timestamp(human_readable, '%Y/%m')
+
+            # Build a list of months not currently held in the database.
+            if link_timestamp > last_month_timestamp:
+                human_readable = human_readable.split('/')
+                months.append((human_readable[0], human_readable[1]))
+
+        months.reverse()  # List months in chronological order.
 
         return months
 
