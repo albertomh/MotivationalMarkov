c36034a4560bbd856f16e730f95af95db07a681e
Alberto Morón Hernández <alberto.moronh@gmail.com>
2018-08-01 07:53:00 +0100

    Return a list of all the titles in the 'articles' table.

--- utilities/Tokeniser.py
+++ utilities/Tokeniser.py
@@ -14,8 +14,20 @@ class Tokeniser:
 
         self.ngram_size = ngram_size
 
+    """
+    Return a list of all the titles in the 'articles' table of the form [(article_id, 'title'), ...].
+    """
+    def all_article_titles(self):
+        sql_all_article_titles = " SELECT id, title FROM articles WHERE title IS NOT NULL "
+        all_article_titles = Database.execute(self.connection, sql_all_article_titles, ()).fetchall()
+
+        # Add a space before punctuation so that it can be tokenised.
+        all_article_titles = [(title[0], title[1].replace('?', ' ?')) for title in all_article_titles]
+
+        return all_article_titles
+
     def main(self):
-        pass
+        self.all_article_titles()
 
 
 if __name__ == "__main__":
