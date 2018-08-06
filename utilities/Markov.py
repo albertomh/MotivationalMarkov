3ad30851461b2dc79e11b286b5a33f99a5d162e6
Alberto Morón Hernández <alberto.moronh@gmail.com>
2018-08-06 08:28:36 +0100

    Create the Markov chain generator.

---
+++ utilities/Markov.py
@@ -0,0 +1,18 @@
+# This software is provided under the MIT license.
+# Copyright (c) 2018 Alberto Morón Hernández
+# --------------------------------------------------------------------------- #
+""" Generate utterances using Markov chains.
+"""
+
+
+class Markov:
+    def __init__(self):
+        pass
+
+    def main(self):
+        pass
+
+
+if __name__ == "__main__":
+    markov = Markov()
+    markov.main()
